766169691fc6bd5f17b240329ed253b99fb5020a
Tex6298 <105018893+Tex6298@users.noreply.github.com>
2022-12-18 00:34:54 +0100

    Update WeatherVane.py

--- modules/WeatherVane.py
+++ modules/WeatherVane.py
@@ -2,11 +2,11 @@ import openai
 import requests
 
 from WeatherVane import gpt3_text_parser
-openai_api = "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"
+
 
 class WeatherVane:
     def __init__(self, openai_api):
-        openai.api_key = openai_api
+        openai.api_key = "openai_api"
         self.input = self.slide_summary_generator
 
     def slide_summary_generator(self):
