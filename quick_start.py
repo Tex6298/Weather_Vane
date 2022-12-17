066e1039d8688dda2a89cc30ab7935ca6d3c6c21
Tex6298 <105018893+Tex6298@users.noreply.github.com>
2022-12-18 00:22:29 +0100

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -39,7 +39,7 @@ if not creds or not creds.valid:
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
 
-OPENAI_API_KEY = API
+OPENAI_API_KEY = "API"
 
 openai.api_key = OPENAI_API_KEY
 
